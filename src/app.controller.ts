a9e040e6968f973946f9520304b7f576b9cde4e6
ChinmayK <kalegaonkar.chinmay1@gmail.com>
2024-12-01 11:35:27 +0530

    add: multer file upload module

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,5 +1,6 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Post, UploadedFile, UseInterceptors } from '@nestjs/common';
 import { AppService } from './app.service';
+import { FileInterceptor } from '@nestjs/platform-express';
 
 @Controller()
 export class AppController {
@@ -8,5 +9,23 @@ export class AppController {
   @Get()
   getHello(): string {
     return this.appService.getHello();
+  }
+
+  @Post('upload')
+  @UseInterceptors(FileInterceptor('file'))
+  uploadFile(@UploadedFile() file: Express.Multer.File) {
+    console.log(file.buffer.toString());
+  }
+
+  @Post('upload/followers')
+  @UseInterceptors(FileInterceptor('file'))
+  uploadFileFollowers(@UploadedFile() file: Express.Multer.File) {
+    console.log(file.buffer.toString());
+  }
+
+  @Post('upload/following')
+  @UseInterceptors(FileInterceptor('file'))
+  uploadFileFollowing(@UploadedFile() file: Express.Multer.File) {
+    console.log(file.buffer.toString());
   }
 }
